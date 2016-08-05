3e8262bdfa6d7f21cc853dba174f0366de3c8320
Jean Azzopardi <jean@jeanazzopardi.com>
2016-08-05 14:30:40 +0200

    Fixes issue where the repository name is different from the framework name which results in a file not found error.

--- RomeBuild/Carthage/Carthage.swift
+++ RomeBuild/Carthage/Carthage.swift
@@ -1,13 +1,30 @@
 import Foundation
 
-func Carthage(args: [String], path: String? = nil) -> Int32 {
+func Carthage(args: [String], path: String? = nil) -> TaskStatus {
     let task = NSTask()
+    let standardOutputPipe = NSPipe()
     task.launchPath = "/usr/local/bin/carthage"
     if let path = path {
         task.currentDirectoryPath = path
     }
     task.arguments = args
+    task.standardOutput = standardOutputPipe
     task.launch()
     task.waitUntilExit()
-    return task.terminationStatus
+
+    let readHandle = standardOutputPipe.fileHandleForReading
+    let data = readHandle.readDataToEndOfFile()
+    if let standardOutput = String.init(data: data, encoding: NSUTF8StringEncoding) {
+        let lines = standardOutput.characters.split { $0 == "\n" || $0 == "\r\n" }.map(String.init)
+        return TaskStatus(status: task.terminationStatus, standardOutput: lines)
+    } else {
+        return TaskStatus(status: task.terminationStatus, standardOutput: nil)
+    }
+}
+
+func getFrameworkPath(taskStatus: TaskStatus) -> String? {    
+    if let lastLineOfOutput = taskStatus.standardOutput?.last {
+        return Helpers().matchesForRegexInText("Created (.*framework.zip)$", text: lastLineOfOutput)?.first
+    }
+    return nil
 }
--- RomeBuild/Utilities/Helpers.swift
+++ RomeBuild/Utilities/Helpers.swift
@@ -2,17 +2,36 @@ import Foundation
 
 struct Helpers {
 
-    func uploadAsset(name: String, revision: String) {
-        
+    func uploadAsset(name: String, revision: String, filePath: String? = nil) {
+        var path: String
+        if let filePath = filePath {
+            path = filePath
+        } else {
+            path = "\(Environment().currentDirectory()!)/\(name).framework.zip"
+        }
+
         do
         {
-            let filePath = "\(Environment().currentDirectory()!)/\(name).framework.zip"
-            Rome().addAsset(name, revision: revision, path: filePath)
-            try NSFileManager.defaultManager().removeItemAtPath(filePath)
+            Rome().addAsset(name, revision: revision, path: path)
+            try NSFileManager.defaultManager().removeItemAtPath(path)
         } catch {
             print(error)
         }
         
     }
+
+    func matchesForRegexInText(regex: String!, text: String!) -> [String]? {
+
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let nsString = text as NSString
+            let results = regex.matchesInString(text,
+                                                options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substringWithRange($0.rangeAtIndex(1))}
+        } catch {
+            return nil
+        }
+    }
+
     
 }
